1d7f83e2e2225f4b7892813e63a579af74c4f426
kalai1212 <jhoncarter555@gmail.com>
2021-10-22 19:19:57 +0530

    New Task

---
+++ src/org/gitt/Gittttt.java
@@ -0,0 +1,18 @@
+package org.gitt;
+
+public class Gittttt {
+	
+	private void emp() {
+		System.out.println("emp details");
+
+	}
+
+	private void empName() {
+		System.out.println("Employee name is:");
+	}
+
+	private void empId() {
+		System.out.println("Employee Id is");
+
+	}
+}
